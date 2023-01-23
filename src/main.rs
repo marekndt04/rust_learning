81aaad007a096ed32060b0a1fd4be0ce966ee02b
Marek Arendt <marek.arendt@yougov.com>
2023-01-23 15:16:37 +0100

    more fun with writing Guessing Game

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::io;
+use std::{io, cmp::Ordering};
+use rand::Rng;
 
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Input Dude:");
 
@@ -12,5 +17,20 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read the line.");
         
-        println!("You got it {guess}")
+        println!("You guessed {guess}");
+    
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+    
+    loop {
+        println!("Please input your guess:");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small!"),
+            Ordering::Greater => println!("To much!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
